fb4cc6e39a717c0db60d3002d0c18357e301e89b
Yannick Lepetit <yanlep@users.noreply.github.com>
2021-12-24 19:43:38 +0100

    désactivation ES

--- src/Entity/Product/ProductVariant.php
+++ src/Entity/Product/ProductVariant.php
@@ -4,22 +4,16 @@ declare(strict_types=1);
 
 namespace App\Entity\Product;
 
-use BitBag\SyliusElasticsearchPlugin\Model\ProductVariantInterface as BitBagElasticsearchPluginVariant;
-use BitBag\SyliusElasticsearchPlugin\Model\ProductVariantTrait;
 use Doctrine\ORM\Mapping as ORM;
 use Sylius\Component\Core\Model\ProductVariant as BaseProductVariant;
 use Sylius\Component\Product\Model\ProductVariantTranslationInterface;
-use Sylius\Component\Core\Model\ProductInterface;
-use Sylius\Component\Core\Model\ProductVariantInterface as BaseProductVariantInterface;
 
 /**
  * @ORM\Entity
  * @ORM\Table(name="sylius_product_variant")
  */
-class ProductVariant extends BaseProductVariant implements BaseProductVariantInterface, BitBagElasticsearchPluginVariant
+class ProductVariant extends BaseProductVariant
 {
-    use ProductVariantTrait;
-
     protected function createTranslation(): ProductVariantTranslationInterface
     {
         return new ProductVariantTranslation();
