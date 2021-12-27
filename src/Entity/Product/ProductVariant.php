ff1789b6a564ebed63e7f9838daf6069072c262b
Yannick Lepetit <yanlep@users.noreply.github.com>
2021-12-27 11:37:10 +0100

    remise en place ES

--- src/Entity/Product/ProductVariant.php
+++ src/Entity/Product/ProductVariant.php
@@ -6,16 +6,25 @@ namespace App\Entity\Product;
 
 use Doctrine\ORM\Mapping as ORM;
 use Sylius\Component\Core\Model\ProductVariant as BaseProductVariant;
+use Sylius\Component\Core\Model\ProductVariantInterface as BaseProductVariantInterface;
+use Sylius\Component\Product\Model\ProductInterface;
 use Sylius\Component\Product\Model\ProductVariantTranslationInterface;
 
 /**
  * @ORM\Entity
  * @ORM\Table(name="sylius_product_variant")
  */
-class ProductVariant extends BaseProductVariant
+class ProductVariant extends BaseProductVariant implements BaseProductVariantInterface
 {
     protected function createTranslation(): ProductVariantTranslationInterface
     {
         return new ProductVariantTranslation();
+    }
+
+    public function getProduct(): ?ProductInterface
+    {
+        $product = parent::getProduct();
+        $product->addVariant($this);
+        return $product;
     }
 }
