1aa1038844ff422fa20da5d8b2c90cb554412bb6
Yannick Lepetit <yanlep@users.noreply.github.com>
2022-02-06 15:39:06 +0100

    up

--- src/Menu/AdminMenuListener.php
+++ src/Menu/AdminMenuListener.php
@@ -33,11 +33,11 @@ final class AdminMenuListener
             ])
             ->setLabel('chullanka_chullis.menu_item')
             ->setLabelAttribute('icon', 'users');
-        $item
+        /* $item
             ->addChild('rma', [
                 'route' => 'app_admin_rma_index',
             ])
             ->setLabel('RMA')
-            ->setLabelAttribute('icon', 'exchange');
+            ->setLabelAttribute('icon', 'exchange'); */
     }
 }
