90a7ef08a7e4a1499e913e9c5c068c2a2d3ba9fc
Yannick Lepetit <yanlep@users.noreply.github.com>
2022-03-08 14:22:04 +0100

    up

--- src/Command/ImportCatalogCommand.php
+++ src/Command/ImportCatalogCommand.php
@@ -431,9 +431,9 @@ class ImportCatalogCommand extends Command
                 $images = explode('|', $article['product_images']);
                 foreach($images as $img)
                 {
-                    // on récupère l'image
-                    if($decodedData = file_get_contents($img))
+                    if(is_file($img))
                     {
+                        $decodedData = file_get_contents($img);// on récupère l'image
                         $tmpPath = sys_get_temp_dir().'/sf_upload'.uniqid();// qu'on place dans le dossier tmp
                         file_put_contents($tmpPath, $decodedData);
 
