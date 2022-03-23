43db3a569d61813b2d7a5a5ea6a17c19c6beaf6b
Yannick Lepetit <yanlep@users.noreply.github.com>
2022-03-23 12:50:14 +0100

    up

--- src/Entity/Product/ProductAttribute.php
+++ src/Entity/Product/ProductAttribute.php
@@ -14,8 +14,42 @@ use Sylius\Component\Product\Model\ProductAttribute as BaseProductAttribute;
  */
 class ProductAttribute extends BaseProductAttribute
 {
+    /**
+     * @ORM\Column(type="boolean", nullable=true)
+     */
+    private $filterable;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=true)
+     */
+    private $preopened;
+
     protected function createTranslation(): AttributeTranslationInterface
     {
         return new ProductAttributeTranslation();
+    }
+
+    public function getFilterable(): ?bool
+    {
+        return $this->filterable;
+    }
+
+    public function setFilterable(?bool $filterable): self
+    {
+        $this->filterable = $filterable;
+
+        return $this;
+    }
+
+    public function getPreopened(): ?bool
+    {
+        return $this->preopened;
+    }
+
+    public function setPreopened(?bool $preopened): self
+    {
+        $this->preopened = $preopened;
+
+        return $this;
     }
 }
