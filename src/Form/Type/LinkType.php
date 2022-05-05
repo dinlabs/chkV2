22b97df033f6f16847f5a45641d06e5f407b3336
Yannick Lepetit <yanlep@users.noreply.github.com>
2022-05-05 22:49:48 +0200

    https://bugs.dinlabs.fr/view.php?id=3171

--- src/Form/Type/LinkType.php
+++ src/Form/Type/LinkType.php
@@ -4,6 +4,8 @@ namespace App\Form\Type;
 
 use App\Entity\Chullanka\Link;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +14,13 @@ class LinkType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('url')
+            ->add('title', TextType::class, [
+                'label' => 'Texte du lien'
+            ])
+            ->add('url', UrlType::class, [
+                'label' => 'Adresse du lien',
+                'default_protocol' => 'https'
+            ])
         ;
     }
 
