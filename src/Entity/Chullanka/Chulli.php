a99c3d63d075cb837e63dfb2543299f56954c8e7
Yannick Lepetit <yanlep@users.noreply.github.com>
2022-06-08 17:38:57 +0200

    https://bugs.dinlabs.fr/view.php?id=3235

--- src/Entity/Chullanka/Chulli.php
+++ src/Entity/Chullanka/Chulli.php
@@ -72,7 +72,8 @@ class Chulli implements ResourceInterface
 
     public function __toString()
     {
-        return $this->firstname . (!empty($this->lastname) ? ' ' . $this->lastname : '');
+        return $this->firstname;
+        //return $this->firstname . (!empty($this->lastname) ? ' ' . $this->lastname : '');
     }
 
     public function getId(): ?int
