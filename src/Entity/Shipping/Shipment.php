0c9b88033da0769e9d7b8f26a0d597119901d2c2
Yannick Lepetit <yanlep@users.noreply.github.com>
2022-07-21 16:30:32 +0200

    https://bugs.dinlabs.fr/view.php?id=3281

--- src/Entity/Shipping/Shipment.php
+++ src/Entity/Shipping/Shipment.php
@@ -13,4 +13,53 @@ use Sylius\Component\Core\Model\Shipment as BaseShipment;
  */
 class Shipment extends BaseShipment
 {
+
+    public function getTrackingUrl(): String
+    {
+        if(!empty($this->tracking))
+        {
+            $shipping_method = $this->getMethod()->getCode();
+            $split_ship = explode('_', $shipping_method);
+            $shipping_method_type = $split_ship[0];
+            $shipping_method_speed = $split_ship[1];
+            
+            if($shipping_method_type == 'home')
+            {
+                if($shipping_method_speed == 'standart')
+                {
+                    if(strlen($this->tracking) > 15)
+                    {
+                        return 'https://trace.dpd.fr/fr/trace/' . $this->tracking;
+                    }
+                    else
+                    {
+                        return 'https://www.laposte.fr/outils/suivre-vos-envois?code=' . $this->tracking;
+                    }
+                }
+                elseif($shipping_method_speed == 'express')
+                {
+                    return 'https://www.chronopost.fr/tracking-no-cms/suivi-page?listeNumerosLT=' . $this->tracking;
+                }
+            }
+            elseif($shipping_method_type == 'pickup')
+            {
+                if($shipping_method_speed == 'standart')
+                {
+                    if(strlen($this->tracking) > 15)
+                    {
+                        return 'https://trace.dpd.fr/fr/trace/' . $this->tracking;
+                    }
+                    else
+                    {
+                        return 'https://www.laposte.fr/outils/suivre-vos-envois?code=' . $this->tracking;
+                    }
+                }
+                elseif($shipping_method_speed == 'express')
+                {
+                    return 'https://www.chronopost.fr/tracking-no-cms/suivi-page?listeNumerosLT=' . $this->tracking;
+                }
+            }
+        }
+        return '';//default
+    }
 }
